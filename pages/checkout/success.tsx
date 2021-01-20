d2280fb03d4bacbcb35752bb72e7c0a94d3e11d2
Brad Rice <bradrice1@gmail.com>
2021-01-19 20:57:33 -0500

    touched up the success page

--- pages/checkout/success.tsx
+++ pages/checkout/success.tsx
@@ -6,10 +6,10 @@ export default function Success (props:any){
     return (
         <Layout>
             <div className="row">
-                <div className="col-sm-8 offset-2">
+                <div className="col-sm-8 offset-sm-2">
                     <h2>Success</h2>
                     <p>Thank you for purchasing my original artwork. I will wrap and send the art work soon. If you have any special instructions, you can use my <Link href="/contact"><a>contact form</a></Link> to send me them.</p>
-                    <p>Return to my <Link href="/art"><a className="btn btn-primary">Artwork</a></Link></p>
+                    <p>Return to my <Link href="/art"><a className="btn btn-light">Artwork</a></Link></p>
 
                 </div>
             </div>
